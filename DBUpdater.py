b59979c5b1820026d8e280081cbf50d925509b03
Miseong Kim <miseongk@naver.com>
2022-06-01 18:08:33 +0900

    first commit

---
+++ DBUpdater.py
@@ -0,0 +1,9 @@
+class DBUpdater:
+    def __init__(self):
+        """생성자: DB 연결 및 종목코드 딕셔너리 생성"""
+
+    def __del__(self):
+        """소멸자: DB 연결 해제"""
+
+if __name__ == '__main__':
+    dbu = DBUpdater()
